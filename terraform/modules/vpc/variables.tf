1c1696a53c1fa1fc7094bc595edf6fd1b3c41aad
JamesLBrocklehurst <jameslbrocklehurst@gmail.com>
2026-06-23 22:43:47 +0100

    adding more resources to the vpc module

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,29 +1,20 @@
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
 
-variable "azs" {
-  description = "List of availability zones"
-  type        = list(string)
+variable "public_subnets" {
+  description = "Map of public subnets with CIDR and AZ"
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
 }
 
 variable "private_subnets" {
-  description = "List of private subnet CIDR blocks"
-  type        = list(string)
-}
-
-variable "public_subnets" {
-  description = "List of public subnet CIDR blocks"
-  type        = list(string)
-}
-
-variable "region" {
-    description = "AWS region for the VPC"
-    type        = string
+  description = "Map of private subnets with CIDR and AZ"
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
 }
--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,24 +1,129 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "6.6.1"
-}
+# VPC
 
-resource "aws_vpc" "var.vpc_name" {
-    cidr_block = var.vpc_cidr
-    region    = var.region
-
-    tags = {
-        Terraform   = "true"
-        Environment = "production"
-    }
-  }
-
- resource "aws_subnet" "Public_Subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnets[0]
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "Main"
+    Terraform   = "true"
+    Environment = "production"
   }
-} 
+}
 
+# Subnets
+
+resource "aws_subnet" "public" {
+  for_each = var.public_subnets
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  tags = {
+    Name        = each.key
+    Terraform   = "true"
+    Environment = "production"
+  }
+}
+
+resource "aws_subnet" "private" {
+  for_each = var.private_subnets
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  tags = {
+    Name        = each.key
+    Terraform   = "true"
+    Environment = "production"
+  }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "main-igw"
+    Terraform   = "true"
+    Environment = "production"
+  }
+}
+
+# EIP + NAT Gateway
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name        = "nat-eip"
+    Terraform   = "true"
+    Environment = "production"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[keys(var.public_subnets)[0]].id
+
+  tags = {
+    Name        = "main-nat"
+    Terraform   = "true"
+    Environment = "production"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+
+# ROUTE TABLES
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "public-rt"
+    Terraform   = "true"
+    Environment = "production"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name        = "private-rt"
+    Terraform   = "true"
+    Environment = "production"
+  }
+}
+
+# -------------------------------------------------------
+# ROUTE TABLE ASSOCIATIONS
+# -------------------------------------------------------
+
+resource "aws_route_table_association" "public" {
+  for_each = var.public_subnets
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = var.private_subnets
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private.id
+}
