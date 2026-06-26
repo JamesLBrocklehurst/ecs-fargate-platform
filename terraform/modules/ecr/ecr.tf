200f2e2194b9673fef9763e6ac60996aaebd402e
JamesLBrocklehurst <jameslbrocklehurst@gmail.com>
2026-06-26 16:36:51 +0100

    finished off the ecr module, including variables and output file. Also updated the example vars file

---
+++ terraform/modules/ecr/ecr.tf
@@ -0,0 +1,44 @@
+# ECR Repository
+
+resource "aws_ecr_repository" "main" {
+  name = "${var.app_name}-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "${var.encryption_type}"
+  
+  }
+
+  tags = {
+    Name        = "${var.app_name}-ecr-repo"
+    Terraform   = "true"
+    Environment = "production"
+  }
+
+}
+
+# ECR Lifecycle Policy
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+}
+
+variable "encryption_type" {
+  description = "The type of encryption to use for the ECR repository"
+  type        = string
+}
