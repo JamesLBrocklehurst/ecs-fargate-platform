4278d889fb5996375bb11181d29f6a649bd158d9
JamesLBrocklehurst <jameslbrocklehurst@gmail.com>
2026-07-04 22:11:07 +0100

    moved config/config.yaml to the correct directory and added force_delete to the ecr setup

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -3,6 +3,7 @@
 resource "aws_ecr_repository" "main" {
   name = "${var.app_name}-ecr-repo"
   image_tag_mutability = "IMMUTABLE"
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
