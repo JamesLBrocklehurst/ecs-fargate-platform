ce9c4e5fb5de83cad2dfd1bd427805822cd4f880
JamesLBrocklehurst <jameslbrocklehurst@gmail.com>
2026-07-07 19:08:51 +0100

    adding s3 bucket backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,4 +6,10 @@ terraform {
       version = "6.51.0"
     }
   }
+  backend "s3" {
+    bucket = "gatus-terraform-state-701632806466"
+    key    = "gatus/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "gatus-terraform-locks"
+  }
 }
